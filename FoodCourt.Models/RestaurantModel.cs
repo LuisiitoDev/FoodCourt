ecd9fcaed9b5c3b39bfe073330b13aeb9ca7cc33
Luis Sandoval Monestel <79773016+LuisiitoDev@users.noreply.github.com>
2023-05-11 19:14:16 -0600

    Initial commit

---
+++ FoodCourt.Models/RestaurantModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodCourt.Models;
+
+public class RestaurantModel
+{
+	public long RestaurantId { get; set; }
+	public string? Name { get; set; }
+	public string? Description { get; set; }
+	public string? Telephone { get; set; }
+	public DateTime CreatedDate { get; set; }
+}
+
---
+++ FoodCourt.Repository/FoodCourtContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using FoodCourt.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodCourt.Repository
+{
+	public class FoodCourtContext : DbContext
+	{
+		public DbSet<RestaurantModel> Restaurants { get; set; }
+	}
+}
+
